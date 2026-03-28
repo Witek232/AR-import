42764c5afb23a5e6501296bddbd309c388faf831
Witek232 <witek232@yandex.com>
2026-03-28 19:01:41 +0100

    Update sanity.ts

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: import.meta.env.8bab26y8,
+  projectId: '8bab26y8',
   dataset:   import.meta.env.SANITY_DATASET || 'production',
   useCdn:    true,
   apiVersion: '2024-01-01',
